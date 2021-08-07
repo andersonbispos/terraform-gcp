54c0bfc477f9b021f2082aef7b6ec2d344bc1a00
andersonbispos <andersonbispos@gmail.com>
2021-08-07 09:52:37 -0300

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,27 +47,27 @@ module "storage" {
   source = "./modules/storage/"
 }
 
-# module "terraform-vpc" {
-#   source = "terraform-google-modules/network/google"
-#   # version = "~> 3.2.2"
+module "terraform-vpc" {
+  source = "terraform-google-modules/network/google"
+  # version = "~> 3.2.2"
 
-#   project_id   = var.project_id
-#   network_name = "terraform-vpc"
-#   routing_mode = "GLOBAL"
+  project_id   = var.project_id
+  network_name = "terraform-vpc"
+  routing_mode = "GLOBAL"
 
-#   subnets = [
-#     {
-#       subnet_name   = "subnet-01"
-#       subnet_ip     = "10.10.10.0/24"
-#       subnet_region = var.region
-#     },
-#     {
-#       subnet_name   = "subnet-02"
-#       subnet_ip     = "10.10.20.0/24"
-#       subnet_region = var.region
-#     }
-#   ]
-# }
+  subnets = [
+    {
+      subnet_name   = "subnet-01"
+      subnet_ip     = "10.10.10.0/24"
+      subnet_region = var.region
+    },
+    {
+      subnet_name   = "subnet-02"
+      subnet_ip     = "10.10.20.0/24"
+      subnet_region = var.region
+    }
+  ]
+}
 
 # resource "google_compute_firewall" "default" {
 #   name    = "tf-firewall"
